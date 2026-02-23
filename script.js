6c54cf51e5b943e7c60443ab587b223857a33c87
IM-Tamim <imtamim1111@gmail.com>
2026-02-23 17:26:18 +0600

    toggling added

--- script.js
+++ script.js
@@ -5,7 +5,13 @@ let allCount = document.getElementById('dash_all');
 let interviewCount = document.getElementById('dash_interview');
 let rejectCount = document.getElementById('dash_rejected');
 
+const allBtn =document.getElementById('all_btn');
+const interviewBtn =document.getElementById('interview_btn');
+const rejectedBtn =document.getElementById('rejected_btn');
+
 const allCards = document.getElementById('allcards');
+
+const mainContainer = document.querySelector('main');
 
 function calculateCount(){
     allCount.innerText = allCards.children.length-1;
@@ -13,3 +19,17 @@ function calculateCount(){
     rejectCount.innerText = rejectedList.length
 }
 calculateCount();
+
+function toggling(id){
+    allBtn.classList.remove('bg-blue-600', 'text-white')
+    interviewBtn.classList.remove('bg-blue-600', 'text-white')
+    rejectedBtn.classList.remove('bg-blue-600', 'text-white')
+
+    allBtn.classList.add('bg-gray-200', 'text-gray-700')
+    interviewBtn.classList.add('bg-gray-200', 'text-gray-700')
+    rejectedBtn.classList.add('bg-gray-200', 'text-gray-700')
+
+    const selected =document.getElementById(id)
+    selected.classList.remove('bg-gray-200', 'text-gray-700')
+    selected.classList.add('bg-blue-600', 'text-white')
+}
